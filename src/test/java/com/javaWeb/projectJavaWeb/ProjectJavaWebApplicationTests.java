bdad9a4c0cecc49359fedfdc76e0aabb28d4f7fc
PedroAC18 <pedro.correa1101@gmail.com>
2023-07-10 17:18:57 -0300

    Criação do Projeto

---
+++ src/test/java/com/javaWeb/projectJavaWeb/ProjectJavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaWeb.projectJavaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
